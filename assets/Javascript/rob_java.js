17acc98dd7bbc8a5e7d55d7fe61e82980996d2ed
Slickrick88 <rickablake@gmail.com>
2018-02-07 19:52:57 -0500

    changes to HTML and JS

--- assets/Javascript/rob_java.js
+++ assets/Javascript/rob_java.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
   var partyName;
   var partyTime;
+  var eventType;
   var address;
   var city;
   var state;
   var host;
   var pplComing;
   var stuff;
+  var attire;
   var addy;
   var timeTill;
   var partyID;
   var counter;
   var timer;
+  var childern;
+  var stuff;
 
   var config = {
     apiKey: "AIzaSyAb-Eg8PzUPHvjSZbD9x6DwLEzUL9Ap_dM",
@@ -30,15 +34,16 @@ $(document).ready(function () {
     event.preventDefault();
     console.log("test2");
     partyName = $("#eventName").val().trim();
+    eventType = $("#eventType").val().trim();
+    attire = $("#attire").val().trim();
     partyTime = $("#datetime").val().trim();
     address = $("#address-input").val().trim();
     city = $("#city-input").val().trim();
+    childern = $("#child").val().trim();
     host = $("#host").val().trim();
     state = $("#state-input").val().trim();
-    pplComing = $("#invitees").val().trim();
-    stuff = $("#items").val().trim();
     addy = address + " " + city + " " + state;
-    
+
     console.log("name " + partyName);
 
     //reset entry form
@@ -48,18 +53,15 @@ $(document).ready(function () {
     database.ref().push({
       Name: partyName,
       Location: addy,
-      Event_Type: "NA",
-      Kid_Friendly: true,
+      Event_Type: eventType,
+      Attire: attire,
+      Kid_Friendly: childern,
       Time: partyTime,
       Host: host,
-      Occurred: true,
-      People: [{
-        guest: pplComing
-      }],
+      Occurred: false,
       Items: [{
-        itemDescription: stuff,
-        itemTaken: false,
-        takeBy: "NA"
+        Who: "NA",
+        what: "NA",
       }]
     });
   });
@@ -79,15 +81,10 @@ $(document).ready(function () {
     var timeTill = moment().diff(moment(partyTime), "days");
     console.log(moment(partyTime).format("MM/DD/YYYY hh:mm:ss"));
     console.log("time until party: " + timeTill);
-    //posts events to the DOM
 
-<<<<<<< HEAD
-    $("#pendingEvents > tbody").append("<tr class='rowID' id='" + partyID + "'><td class=partyTime'>" + partyTime + "</td><td class='partyName'>" + partyName + "</td><td class=address>" +
-      addy + "</td><td class=host>" + host + "</td><td class='minutesTill'>" + timeTill + "</td></tr>");
-=======
-    $("#pendingEvents > tbody").append("<tr class='rowID' id='" + counter + "' data-key='" + partyID + "'><td>" + (moment(partyTime).format("MM/DD/YYYY hh:mm:ss")) + "</td><td class='partyID'>" + partyName + "</td><td>" +
-      addy + "</td><td>" + host + "</td><td class='minutesTill'>" + timeTill + "</td></tr>");
->>>>>>> master
+    //posts events to the DOM
+    $("#pendingEvents > tbody").append("<tr class=rowID id='" + partyID + "'><td class=eventTime>" + (moment(partyTime).format("MM/DD/YYYY hh:mm:ss")) + "</td><td class=partyID>" + partyName + "</td><td class=location>" +
+      addy + "</td><td class=host >" + host + "</td><td class=minutesTill>" + timeTill + "</td></tr>");
 
   });
 
@@ -103,41 +100,16 @@ $(document).ready(function () {
   function updateTime() {
     timer = setInterval(partyTime, 1 * 1000)
   };
- 
-  var table=$("#pendingEvents").DataTable();
+
+  var table = $("#pendingEvents").DataTable();
   $("#pendingEvents tbody").on("click", "tr", function () {
-    var rowData = table.row($(this).parents("tr").data());
-    console.log("row data:" + rowData);
-    $("#itemsTbl tbody tr").remove()
+    //remove items the were in the table previously
+    $("#itemsTbl tbody tr").remove();
+    //set a variable = to the row data
+    var rowData = table.row(this).data();
+    console.log(table.row(this).data());
     var database = firebase.database();
-    var key ="";
-    key = this.id;
+    var key = rowData.id;
     console.log("key is: " + key);
-    database.ref(key + '/Host').once('value').then(function (snapshot) {
-      host = snapshot.val();
-      console.log("host pulled: " + host);
-    }, function (error) {
-      console.log(error);
-    });
-    database.ref(key + '/Items').on("child_added", function (childSnapshot) {
-      var itemKey = childSnapshot.val().key;
-      var itemDescription = childSnapshot.val().itemDescription;
-      var itemTaken = childSnapshot.val().itemTaken;
-      if (itemTaken === true) {
-        var itemTakenDesc = "Claimed"
-      }
-      else {
-        var itemTakenDesc = "Open"
-      };
-      var takenBy = childSnapshot.val().takeBy;
-      console.log("items pulled: " + itemDescription);
-      console.log("items take?: " + itemTaken);
-      console.log("taken by whom: " + takenBy);
-      //add to html dom to see what everyone is bringing
-      $("#itemsTbl > tbody").append("<tr class='rowID' data-key='" + itemKey + "'><td>" + itemDescription + "</td><td class='" +
-        itemTaken + "'>" + itemTakenDesc + "</td><td>" + takenBy + "</td></tr>");
-    }, function (error) {
-      console.log(error);
-    });
   });
 });
