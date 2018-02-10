fd553f12454cdf734bac51c39c4406a7c0e09a76
Slickrick88 <rickablake@gmail.com>
2018-02-10 10:22:51 -0500

    pushing active firebase pulls

--- assets/Javascript/rob_java.js
+++ assets/Javascript/rob_java.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
   var city;
   var state;
   var host;
-  var pplComing;
+  var who;
+  var what;
   var stuff;
   var attire;
   var addy;
@@ -16,6 +17,9 @@ $(document).ready(function () {
   var timer;
   var childern;
   var stuff;
+  var description;
+  var key;
+  var reference;
 
   var config = {
     apiKey: "AIzaSyAb-Eg8PzUPHvjSZbD9x6DwLEzUL9Ap_dM",
@@ -42,6 +46,7 @@ $(document).ready(function () {
     childern = $("#child").val().trim();
     host = $("#host").val().trim();
     state = $("#state-input").val().trim();
+    description = $("#descripction").val().trim();
     addy = address + " " + city + " " + state;
 
     console.log("name " + partyName);
@@ -53,6 +58,7 @@ $(document).ready(function () {
     database.ref().push({
       Name: partyName,
       Location: addy,
+      Description: description,
       Event_Type: eventType,
       Attire: attire,
       Kid_Friendly: childern,
@@ -60,8 +66,6 @@ $(document).ready(function () {
       Host: host,
       Occurred: false,
       Items: [{
-        Who: "NA",
-        what: "NA",
       }]
     });
   });
@@ -85,27 +89,104 @@ $(document).ready(function () {
     console.log(moment(partyTime).format("MM/DD/YYYY hh:mm:ss"));
     console.log("time until party: " + timeTill);
 
-    if (timeTill === -1)  {
+    if (timeTill === -1) {
       $(".rowID").val("");
     };
- 
+
     //posts events to the DOM
     $("#pendingEvents > tbody").append("<tr class='rowID' data-key='" + partyID + "'><td class=partyTime'>" + (moment(partyTime).format("MM/DD/YYYY hh:mm:ss")) + "</td><td class='partyName'>" + partyName + "</td><td class=address>" +
       addy + "</td><td class=host>" + host + "</td><td class='minutesTill'>" + timeTill + "</td></tr>");
 
   });
 
- 
-  var table=$("#pendingEvents").DataTable();
+
+  var table = $("#pendingEvents").DataTable();
   $("#pendingEvents tbody").on("click", "tr", function () {
     //remove items the were in the table previously
     $("#itemsTbl tbody tr").remove();
     //gets the key from the row that is clicked so it can retrieve data from firebase
-    var key = $(this).data("key");
-    console.log("testing: "+key); 
-    
-    database.ref(key+"");
-
-    
+    key = $(this).data("key");
+    console.log("testing: " + key);
+    //retrieves clicked record from database
+    reference = database.ref('/' + key);
+    // attire= attireFun(reference.val().Attire);
+    // console.log( reference);
+    reference.child("Items").once('value', gotData);
+    // function that loops through items
+    function gotData(snapshot) {
+      console.log(snapshot.val());
+      snapshot.forEach(function(itemSnapshot) {
+        var I = itemSnapshot.key;
+        console.log("item key: " + I);
+        var person = itemSnapshot.val().Who;
+        console.log("whp: " + person);
+        var stuff = itemSnapshot.val().what;
+        console.log("what: " + what);
+        $("#whoWhat > tbody").append("<tr class='itemID' data-key='" + I + "'><td class=who'>" + person + "</td><td class='what'>" + stuff + "</td></tr>");
+      });
+    };
+    reference.child("Attire").once('value', function(snap) {console.log(snap.val())});
   });
-});
+
+  //add new data to party item
+  $("#itemAdd").on("click", function (event) {
+    console.log("key in item add: " + key);
+    //assign variables from HTML
+    who = $("#who").val().trim();
+    what = $("#what").val().trim();
+    //follow path in firebase to items
+    reference = database.ref('/' + key +'/Items');
+    reference.push({
+      Who: who,
+      What: what,
+    });
+  });
+
+
+    function attireFun(cloths) {
+      console.log("cloths: " + cloths);
+      if (cloths == 1) {
+        return "Casual";
+      } else if (cloths == 2) {
+        return "Dressy Casual";
+      } else if (cloths == 3) {
+        return "Business Casual";
+      } else if (cloths == 4) {
+        return "Semiformal";
+      } else if (cloths == 5) {
+        return "Black Tie";
+      } else if (cloths == 6) {
+        return "White Tie";
+      } else {
+        "Other"
+      };
+    };
+
+    function eventTypeFunc(type) {
+      console.log("event Type: " + type);
+      if (cloths == 1) {
+        return "Block Party";
+      } else if (cloths == 2) {
+        return "Birthday Party";
+      } else if (cloths == 3) {
+        return "Shhhh! Surprise Birthday Party";
+      } else if (cloths == 4) {
+        return "Cocktail Party";
+      } else if (cloths == 5) {
+        return "Dinner Party";
+      } else if (cloths == 6) {
+        return "Housewarming Party";
+      } else {
+        "Other"
+      };
+    };
+
+    function kidsFunc(kids) {
+      console.log("kids: " + kids);
+      if (cloths == 1) {
+        return "Children allowed";
+      } else {
+        return "No Kids!";
+      };
+    };
+  });
